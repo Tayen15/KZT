09735e5f6f34633878694d80592dc40c23e280cd
Tayen15 <tiantiantitik@gmail.com>
2022-06-27 06:34:54 +0700

    Added ownerID

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,7 +1,4 @@
-const discord = require('discord.js')
-const cooldowns = new discord.Collection();
-
-const { prefix } = require('../config');
+const { prefix, ownerID } = require('../config')
 
 module.exports = {
     name: "messageCreate",
@@ -17,23 +14,13 @@ module.exports = {
 
         if (!cmd) return;
 
-        if (!client.cooldowns.has(command.name)) {
-            client.cooldowns.set(command.name, new discord.Collection());
+        const commandName = cmd.name.toLowerCase();
+
+        if (cmd.ownerOnly) {
+            if (message.author.id !== ownerID) return
+            message.channel.send("You dont have permissions to use this command!");
         }
 
-        let now = Date.now();
-        let timeStamp = client.cooldowns.get(command.name);
-        let cool = command.cooldown || 5;
-        let userCool = timeStamp.get(message.author.id) || 0;
-        let estimated = userCool + cool * 1000 - now;
-
-        if (userCool && estimated > 0) {
-            return message.reply(`Please waith ${( estimated / 1000 ).toFixed()}second(s) before reusing the *${command.name}* command.`)
-            .then(message => message.delete({ timeout: estimated }));
-        }
-        
-        timeStamp.set(message.author.id, now);
-        client.cooldowns.set(command.name, timeStamp);
         try {
             cmd.execute(client, message, args);
             client.CommandsRan++;
@@ -42,7 +29,7 @@ module.exports = {
             console.log(e);
             message.reply('An error occurred while executing the command!');
         } finally {
-            console.log(`User: ${message.author.tag} | command: ${command.name}`);
+            console.log(`User: ${message.author.tag} | command: ${commandName}`);
         };
     },
 };
