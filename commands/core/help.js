e3af438e001836642f94119b73bc4d2849786c1a
Tayen15 <tiantiantitik@gmail.com>
2022-06-27 07:14:07 +0700

    Fix MessageEmbed and bug

--- commands/core/help.js
+++ commands/core/help.js
@@ -17,24 +17,39 @@ module.exports = {
 
             const helpEmbed = new MessageEmbed()
             .setAuthor({
-                name: `${client.user.username} Commands`,
+                name: `${client.user.username}`,
                 iconURL: 'https://cdn.discordapp.com/avatars/785398919923892264/fe7115806c2f0e77d9a999bfdf79d408.png'
             })
             .addFields(
-                { name: 'Info', value: `${infor}`, inline: false },
+                { name: 'Info', value: `${infor}`, inline: true },
                 { name: 'Music', value: `${musik}`, inline: false }
+
             )
             .setFooter({
-                text: `For more information on a command try: ${prefix}help <command>`
+                text: `For more information an a command: ${prefix}help <command>`
             })
-            message.channel.send({ embeds: [helpEmbed] });
-
+            message.channel.send({ embeds: [helpEmbed] })
         } else {
             const command = message.client.commands.get(args.join(" ").toLowerCase()) || message.client.commands.find(x => x.aliases && x.aliases.includes(args.join(" ").toLowerCase()));
 
             if (!command) return message.channel.send(
                 `Cannot find command: **${args}**`
             );
+
+            const moreHelp = new MessageEmbed()
+            .setAuthor({
+                name: `Command ${command.name}`
+            })
+            .addFields(
+                { name: 'Category', value: `${command.category}`, inline: true },
+                { name: 'Aliases', value: `${command.aliases.length < 1 ? 'None' : command.aliases.join(', ')}`, inline: false },
+                { name: 'Usage', value: `${command.utilisation.replace('{prefix}', prefix)}`, inline: false }
+            )
+            .setDescription(`${command.description}`)
+            .setFooter({
+                text: `${message.author.username}`
+            })
+            message.channel.send({ embeds: [moreHelp] });
         }
     },
 };
