d9e9f60b9c2db5f32ff5c73fab32a039c2fb4922
Tayen15 <tiantiantitik@gmail.com>
2022-06-27 07:14:30 +0700

    Fix category name

--- commands/music/play.js
+++ commands/music/play.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "play",
     description: "Play a song or playlist",
     aliases: ['p'],
-    category: "Music",
+    category: "music",
     usage: "{prefix}play",
     cooldown: 5,
     execute(client, message, args) {
