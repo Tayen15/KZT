7392c9b5a21de2d5923149532ec09142a07ae7c8
Tayen15 <tiantiantitik@gmail.com>
2022-06-27 07:49:10 +0700

    Add reload command

---
+++ commands/dev/reload.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name: 'reload',
+    description: 'Reloads a specific file or command',
+    aliases: ['r'],
+    category: 'dev',
+    execute(client, message, args) {
+        if(!args[0]) return message.channel.send('Please include the command you want to reload').then(message => message.delete({ timeout:5000 }));
+        let commandName = args[0].toLowerCase();
+
+        let directory;
+        try {
+            delete require.cache[require.resolve(`../info/${commandName}.js`)];
+            directory = "info";
+        } catch {
+            try {
+                delete require.cache[require.resolve(`../music/${commandName}.js`)];
+                directory = "music";
+            } catch {
+                try {
+                    delete require.cache[require.resolve(`../core/${commandName}.js`)];
+                    directory = "core";
+                } catch {
+                    try {
+                        delete require.cache[require.resolve(`../dev/${commandName}.js`)];
+                        directory = "dev";
+                    } catch {
+                        return message.channel.send('The command was not found!');
+                    }
+                }
+            }
+        }
+
+        client.commands.delete(commandName);
+        const pull = require(`../${directory}/${commandName}.js`);
+        client.commands.set(commandName, pull);
+        message.channel.send(`Command \**${args[0].toUpperCase()}\** has been reloaded.`);
+        console.log(`Command: ${args[0].toUpperCase()} Reload By: ${message.author.id}`);
+    }
+}
