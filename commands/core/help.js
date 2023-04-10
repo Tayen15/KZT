7ba1c1d68977e3efb373b9844faa02ac6385054f
Tayen15 <tiantiantitik@gmail.com>
2023-04-10 23:08:58 +0700

    Same edit help.js Message Embed

--- commands/core/help.js
+++ commands/core/help.js
@@ -39,7 +39,7 @@ module.exports = {
                 `Cannot find command: **${args}**`
             );
 
-            const moreHelp = new MessageEmbed()
+            const moreHelp = new EmbedBuilder()
             .setAuthor({
                 name: `Command ${command.name}`
             })
