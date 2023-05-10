fc930977170349e51126ce03da86d2b34d69c546
Tayen15 <tiantiantitik@gmail.com>
2023-05-10 08:42:31 +0700

    FIx command setactivity

--- commands/dev/setactivity.js
+++ commands/dev/setactivity.js
@@ -1,43 +1,42 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Permissions } = require('discord.js');
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = {
      data: new SlashCommandBuilder()
-          .setName('setactivity')
-          .setDescription('Set the bot activity')
-          .addStringOption(option =>
-               option.setName('type')
-                    .setDescription('The type of activity')
-                    .setRequired(true)
-                    .addChoice(
-                         { name: 'Playing', value: 'PLAYING' },
-                         { name: 'Streaming', value: 'STREAMING'},
-                         { name: 'Listening', value: 'LISTENING'},
-                         { name: 'Watching', value: 'WATCHING'},
-                         { name: 'Competing', value: 'COMPETING'},
-                    ))
-          .addStringOption(option =>
-               option.setName('name')
-                   .setDescription('The name of the activity')
-                   .setRequired(true)
-          ),
+     .setName('setactivity')
+     .setDescription('Set the bot activity')
+     .addStringOption(option =>
+          option
+          .setName('type')
+          .setDescription('The type of activity')
+          .setRequired(true)
+          .addChoices(
+               { name: 'Playing', value: 'PLAYING'},
+               { name: 'Streaming', value: 'STREAMING'},
+               { name: 'Listening', value: 'LISTENING'},
+               { name: 'Watching', value: 'WATCHING'},
+               { name: 'Competing', value: 'COMPETING'}
+          )
+     )
+     .addStringOption(option =>
+          option
+          .setName('name')
+          .setDescription('The name of the activity')
+          .setRequired(true)
+     ),
      name: 'setactivity',
-     description: 'Set activity presence status',
-     aliases: [''],
      category: 'dev',
 
      async execute(interaction) {
-          if (!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+          if (!interaction.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
                return interaction.reply('You do not have permission to use this command.');
-          }
+          }     
 
-          const type = interaction.options.getString('type');
-          const name = interaction.options.getString('name');
+          const type = interaction.options.getString('type', true);
+          const name = interaction.options.getString('name', true);
 
           try {
-               await interaction.client.user.setPresence({
-                   activities: [{ name: name, type: type }]
-               });
+               await interaction.client.user.setActivity(name, { type });
                await interaction.reply({ content: `Activity set to ${type} ${name}.`, ephemeral: true });
           } catch (error) {
                console.error(error);
