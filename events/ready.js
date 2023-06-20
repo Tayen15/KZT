105f52f331c43e9db7f5be88b8988279d0353d54
Tayen15 <tiantiantitik@gmail.com>
2023-06-20 22:02:10 +0700

    Presence can check if server mc online/offline dan install library

--- events/ready.js
+++ events/ready.js
@@ -1,29 +1,45 @@
-const config = require('../config');
 const { Events, ActivityType } = require('discord.js');
-const adhan = require('namaz');
 const moment = require('moment-timezone');
+const fetch = require('axios');
 
-const date = new Date();
-var coordinates = new adhan.Coordinates(-6.418145, 106.862089);
-var params = adhan.CalculationMethod.Singapore();
-var precisionOn = true;
-params.madhab = adhan.Madhab.Hanafi;
-var prayerTimes = new adhan.PrayerTimes(coordinates, date, params, precisionOn);
+const serverStatusURL = 'https://api.mcstatus.io/v2/';
 
-var fajrTime = moment(prayerTimes.fajr).tz('Asia/Jakarta').format('h:mm:ss A');
-var maghribTime  = moment(prayerTimes.maghrib).tz('Asia/Jakarta').format('h:mm:ss A');
+async function getServerStatus() {
+    try {
+        const response = await fetch(serverStatusURL);
+        const data = await response.json();
+        return data.online;
+    } catch (error) {
+        console.error('Failed to fetch server status:', error);
+        return false;
+    }
+}
 
 module.exports = {
-    name: Events.ClientReady,   
+    name: Events.ClientReady,
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log('%s is online: %s servers, and %s members', client.user.username, client.guilds.cache.size, client.users.cache.size);
 
         const statusType = ActivityType.Listening;
-        client.user.setPresence({
-            activities: [
-               { name: 'PANAS ANJING', type: statusType }
-            ]
-        });
+        async function updatePresence() {
+            const serverStatus = await getServerStatus();
+            const currentTime = moment().tz('Asia/Jakarta').format('h:mm:ss A');
+            let presenceActivity;
+
+            if (serverStatus) {
+                presenceActivity = `Online Players: ${serverStatus}`;
+            } else {
+                presenceActivity = 'Wandek Selalu Dihati';
+            }
+
+            client.user.setPresence({
+                activities: [{ name: presenceActivity, type: statusType }]
+            });
+
+            console.log(`Presence updated at ${currentTime}: ${presenceActivity}`);
+        }
+
+        setInterval(updatePresence, 60000);
     }
 };
