f0dc03594a34cb5d4c7216138896214abb53bc9d
Tayen15 <tiantiantitik@gmail.com>
2025-04-20 11:48:12 +0700

    Refactor lofiReconnect handler to reload stream resource if the current stream is down, improving reliability of the lofi music playback

--- handlers/lofiReconnect.js
+++ handlers/lofiReconnect.js
@@ -1,7 +1,8 @@
 const { getLofiSessions } = require('../utils/lofiStorage');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, NoSubscriberBehavior } = require('@discordjs/voice');
 
-const STREAM_URL = 'https://stream-157.zeno.fm/0r0xa792kwzuv?zt=...'; // pakai URL milikmu
+const STREAM_URL = 'https://stream-157.zeno.fm/0r0xa792kwzuv?zt=...';
+const RECONNECT_INTERVAL = 30000; // 30 detik
 
 module.exports = async (client) => {
      const sessions = getLofiSessions();
@@ -33,6 +34,25 @@ module.exports = async (client) => {
                });
 
                connection.subscribe(player);
+
+               setInterval(async () => {
+                    try {
+                         const status = player.state.status;
+                         const res = await fetch(STREAM_URL, { method: 'HEAD' });
+               
+                         if (status !== AudioPlayerStatus.Playing || !res.ok) {
+                              console.log(`[LofiReconnect] Stream seems down or bot not playing, reloading stream...`);
+               
+                              const newResource = createAudioResource(STREAM_URL, {
+                                   inlineVolume: true
+                              });
+                              newResource.volume.setVolume(0.5);
+                              player.play(newResource);
+                         }
+                    } catch (err) {
+                         console.error('[LofiReconnect] Auto-reconnect check failed:', err.message);
+                    }
+               }, RECONNECT_INTERVAL);
 
                console.log(`[LofiReconnect] Reconnected to ${guild.name} - #${channel.name}`);
           } catch (err) {
